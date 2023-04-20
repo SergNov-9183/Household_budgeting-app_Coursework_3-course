1e2ea3676cd42094a42ea2b8792af10dd8574088
Sergey Novoseltsev <75534548+SergNov-9183@users.noreply.github.com>
2023-04-20 16:50:35 +0300

    initial commit
    initial commit - ubdate graddle file_module app

---
+++ app/src/main/java/com/example/coursework_3course_budgetapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.coursework_3course_budgetapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
